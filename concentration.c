5605008313ceac2e7b1af14891c92a73133a664e
kellyzc <kellyzc@rose-hulman.edu>
2020-02-11 10:41:37 -0500

    Initial commit

---
+++ concentration.c
@@ -0,0 +1,15 @@
+/* Concentration
+ * ECE230-02 Final Project
+ * Written by Zach Kelly and Tanner Brammeier
+ * 2/11/20
+ * 
+ * Description:
+ * This program allows two players using separate boards to play the game
+ * concentration using serial communication.
+ */
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
